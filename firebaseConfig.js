9697bc687d678e66c3b36b173d7bd145297fdb1d
Ignacio <100495680@alumnos.uc3m.es>
2024-10-10 20:21:37 +0400

    Save the changes in the firebase.

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBY2EBA0KnOYZDjxCwSHBMGcLaFjG6IL4o",
+    authDomain: "greenify-e91f0.firebaseapp.com",
+    projectId: "greenify-e91f0",
+    storageBucket: "greenify-e91f0.appspot.com",
+    messagingSenderId: "995349170683",
+    appId: "1:995349170683:web:10bc48311fbf4a2710235e",
+    measurementId: "G-1FP8G4JTGR"
+  };
