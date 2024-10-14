fb2878cb56ac57a61c13a3d71cfe6e2a0ef329e1
Ignaciol0 <117018561+Ignaciol0@users.noreply.github.com>
2024-10-14 22:05:45 +0400

    Delete DB directory

--- DB/index.js
+++
@@ -1,8 +0,0 @@
-import { insertEmptyProjects } from './insert.js';
-
-// Call the function to insert empty projects
-insertEmptyProjects().then(() => {
-  console.log('All projects inserted successfully');
-}).catch((error) => {
-  console.error('Error inserting projects:', error);
-});
